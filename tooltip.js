afa9f8de7552a33b899f9ffb503911a44b483a89
LeeJihyun99 <ljihyun99.german@gmail.com>
2023-12-12 17:04:32 +0100

    created my own DOM/template to make it reusable easily

--- tooltip.js
+++ tooltip.js
@@ -4,17 +4,29 @@ class Tooltip extends HTMLElement{
         this._tooltipBox;
         this._tooltipText = 'This is a dummy text.';
         this.attachShadow({mode:'open'});
+        this.shadowRoot.innerHTML=
+        `
+        <style>
+            div{
+                background-color: black;
+                color:white;
+                position:absolute;
+                z-index:10;
+            }
+        </style>
+        <slot>
+            This will be shown only when there is nothing in between the opening and closing custom tags.
+        </slot>
+        <span>XD</span>`;
 
     }
     connectedCallback(){
-        const tooltipIcon = document.createElement('span');
         if(this.hasAttribute('text')){
             this._tooltipText = this.getAttribute('text');
         }
-        tooltipIcon.textContent = 'XD';
+        const tooltipIcon = this.shadowRoot.querySelector('span');
         tooltipIcon.addEventListener('mouseenter', this._showTooltip.bind(this));
         tooltipIcon.addEventListener('mouseout',this._hideTooltip.bind(this));
-        this.shadowRoot.appendChild(tooltipIcon);
         this.style.position = 'relative';
     }
     _showTooltip(){
